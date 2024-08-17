0b85be455217d28b33c084dcea56ac6213664702
Denis <denis.israfilov2002@mail.ru>
2024-08-18 00:24:04 +0500

    Префикс api/ в урлах

--- config/urls.py
+++ config/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    path("users/", include("users.urls")),
 ]
+
+urlpatterns = [path(f'api/', include(urlpatterns))]
