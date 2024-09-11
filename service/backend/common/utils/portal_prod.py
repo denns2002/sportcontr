3b1e932aa98d3526e2c37a76806e2a15b39b54d2
Denis <denis.israfilov2002@mail.ru>
2024-09-11 15:27:32 +0500

    функция создания, обновление реги, обновление создания порталов

--- service/backend/common/utils/portal_prod.py
+++ service/backend/common/utils/portal_prod.py
@@ -7,12 +7,19 @@ from config.settings import BASE_DIR
 def read_vm_ip(lines):
     for i, v in enumerate(lines):
         if "one_to_one_nat:" in v:
-            return lines[i+1][lines[i+1].find(': ') + 2:]
+            return lines[i+1][lines[i+1].find(': ') + 2:].rstrip()
 
 
-def change_docker_compose_conf(user_email: str, new_conf: str = 'new-docker-compose.yml'):
+def change_docker_compose_conf(user, new_conf: str = 'new-docker-compose.yml'):
     lines = open_file('docker-compose.yml')
-    lines.insert(11, f'            - ADMINEMAIL={user_email}\n')
+    fields = {
+        'ADMIN_LOGIN': user.username,
+        'ADMIN_EMAIL': user.email,
+        'ADMIN_FIRST_NAME': user.first_name,
+        'ADMIN_LAST_NAME': user.last_name,
+    }
+    for f, v in fields:
+        lines.insert(11, f'            - {f}={v}\n')
 
     with open(f'{BASE_DIR / new_conf}', 'w', encoding='utf-8') as f:
         f.writelines(lines)
